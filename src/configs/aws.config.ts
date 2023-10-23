018bea70b6a476c91844feff4b478c4cc8b6c7ff
Mihir Verma <mihir.verma@itential.com>
2023-10-23 23:26:31 +0530

    [CM-5] removed unwanted consoles

--- src/configs/aws.config.ts
+++ src/configs/aws.config.ts
@@ -2,12 +2,6 @@ import AWS from "aws-sdk";
 import { S3Client } from "@aws-sdk/client-s3";
 import config from "./environment.config";
 
-// AWS.config.update({
-//   region: "us-west-2",
-//   accessKeyId: config.getAWSAccessKeyId(),
-//   secretAccessKey: config.getAWSSecretAccessKey(),
-// });
-
 export const s3 = new S3Client({
   region: "us-west-2",
   credentials: {
